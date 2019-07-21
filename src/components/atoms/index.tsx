4ada0caa618cce94121882fe6d6f9fbcf2a9ad65
Grekz <vidbla.grekz@gmail.com>
2019-07-21 23:50:48 +0200

    Update index for components

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,3 +1,5 @@
-export { default as Text } from './Text'
+export { default as Avatar } from './Avatar'
 export { default as Button } from './Button'
 export { default as Card } from './Card'
+export { default as Divider } from './Divider'
+export { default as Text } from './Text'
